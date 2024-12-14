b932108d444afa009b6058b3e3d952d540c0ef70
Mehirion <Mehirion1@gmail.com>
2024-12-14 17:32:08 +0100

    Dodanie komponentów

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="bg-gray-800 text-white p-2 text-center">
+      <p>© 2024 FootScore</p>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/components/TeamList.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const TeamList = () => {
+  return (
+    <div className="p-4">
+      <h2 className="text-lg font-semibold mb-4">Twoje ulubione drużyny:</h2>
+      <ul className="list-disc list-inside">
+        <li>Przykładowa Drużyna 1</li>
+        <li>Przykładowa Drużyna 2</li>
+      </ul>
+    </div>
+  );
+};
+
+export default TeamList;
