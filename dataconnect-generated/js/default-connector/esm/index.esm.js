d099326d0f10bdcd6564488f482bf8d1783b0e07
Mehirion <Mehirion1@gmail.com>
2025-03-03 21:32:45 +0100

    Udany deploy FireBase

---
+++ dataconnect-generated/js/default-connector/esm/index.esm.js
@@ -0,0 +1,7 @@
+
+export const connectorConfig = {
+  connector: 'default',
+  service: 'footscore',
+  location: 'us-central1'
+};
+
---
+++ dataconnect-generated/js/default-connector/index.cjs.js
@@ -0,0 +1,8 @@
+
+const connectorConfig = {
+  connector: 'default',
+  service: 'footscore',
+  location: 'us-central1'
+};
+exports.connectorConfig = connectorConfig;
+
